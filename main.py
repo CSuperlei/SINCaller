b87f585c12fe4abb4ff1b0a55168548db3664db8
CSuperlei <csuperlei@163.com>
2020-06-01 23:40:22 -0400

    2020-06-01

--- main.py
+++ main.py
@@ -75,13 +75,16 @@ def main():
             return
 
     elif int(mode) == 2:  ## 测试
-        load_filename = args.load
-        if load_filename is not None:
-            test_data = np.load(load_filename, allow_pickle=True)
-
-        if test_data is not None and int(args.test) == 1:
-            generator_params = { 'shuffle': False }
-            sendin = testing(test_data, test_model=1, generator_params=generator_params)
+        if int(args.test) == 1:
+            load_filename = args.load
+            if load_filename is not None:
+                samples_data = np.load(load_filename, allow_pickle=True)
+            else:
+                print('load filename is empyt')
+                return
+            if samples_data is not None and int(args.test) == 1:
+                generator_params = { 'shuffle': False }
+                sendin = testing(samples_data, test_model=1, generator_params=generator_params)
 
         elif int(args.test) == 2:
             d = {'aa': 1, 'at': 2, 'ac': 3, 'ag': 4,
