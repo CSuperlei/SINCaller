6c703ad530c7c7d097b205882b0553436f29185a
CSuperlei <csuperlei@163.com>
2020-06-17 13:17:49 -0400

    2020-06-17

--- model/scSNV_model.py
+++ model/scSNV_model.py
@@ -1,15 +1,15 @@
-from keras.models import Model
-from keras.optimizers import Adam, RMSprop
-from keras.layers import Input, Embedding, LSTM, Bidirectional, Dense, BatchNormalization
-from keras.layers import Dropout, Lambda
-from keras.utils import multi_gpu_utils
-from keras.utils.vis_utils import plot_model
+# from keras.models import Model
+# from keras.optimizers import Adam, RMSprop
+# from keras.layers import Input, Embedding, LSTM, Bidirectional, Dense, BatchNormalization
+# from keras.layers import Dropout, Lambda
+# from keras.utils import multi_gpu_utils
+# from keras.utils.vis_utils import plot_model
 
-# from tensorflow.keras.layers import *
-# from tensorflow.keras.models import Model
-# from tensorflow.keras.optimizers import Adam, RMSprop
-# from tensorflow.keras.utils import plot_model
-# from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam, RMSprop
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.layers import Embedding
 
 
 import tensorflow as tf
