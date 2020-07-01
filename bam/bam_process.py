090785a64e43382bb3d68cf05aa0866fe94a1005
CSuperlei <csuperlei@163.com>
2020-06-30 22:12:13 -0400

    2020-06-30

--- bam/bam_process.py
+++ bam/bam_process.py
@@ -17,11 +17,11 @@ class BAM:
             if rec.pos == start - 1:
                 # print(rec.get_mapping_qualities())
                 # print(rec.get_query_sequences())
-                base_list = rec.get_query_sequences()
                 # print(rec.get_query_positions())
                 # print(rec.reference_pos)
                 # print(dir(rec))
                 # print(dir(rec.pileups))
+                base_list = rec.get_query_sequences()
                 indel_list = [int(tmp.indel) for tmp in rec.pileups]
                 # print(indel_list)
                 # for tmp in rec.pileups:
@@ -33,31 +33,29 @@ class BAM:
                 #     print(tmp.alignment.reference_name)
                 #     print(tmp.alignment.mapping_quality)
 
-                indel_deletion = '0'
-                indel_insertion = '0'
-
+                re = '0'
                 sum_indel_list = sum(indel_list)
                 if sum_indel_list == 0:  ## 如果是SNP
-                    base_ad = Counter(base_list)
-                    ad = []  ## 计算不同等位基因数量
-                    for k, v in base_ad.items():
-                        if k != '':
-                            ad.append(v)
-                    ad = ",".join(ad)
-                    dp = rec.n ## 计算Coverage深度
+                    bl = rec.get_query_sequences()
+                elif sum_indel_list < 0:
+                    bl = rec.next.get_query_sequences()
+                    indel_index = np.argmin(indel_list)
+                    indel_value = np.min(indel_list)
+                    re = self.fetch_row(bam_file, chr_id, rec.pos, rec.pos + 1, indel_index, indel_value)
+                elif sum_indel_list > 0:
+                    bl = rec.next.get_query_sequences()
+                    indel_value = np.max(indel_list)
+                    indel_index = np.argmax(indel_list)
+                    re = self.fetch_row(bam_file, chr_id, rec.pos, rec.pos + 1, indel_index, indel_value)
 
-                if sum_indel_list < 0:
+                base_ad = Counter(bl)
+                ad = []  ## 计算不同等位基因数量
+                for k, v in base_ad.items():
+                    ad.append(v)
+                dp = sum(ad) ## 总的映射深度
+                ad = ",".join(ad) ## 每种等位基因的深度
 
-                    indel_deletion = np.min(indel_list)
-
-                if sum_indel_list > 0:
-                    insertion = np.max(indel_list)
-                    index = np.argmax(indel_list)
-                    re = self.fetch_row(bam_file, chr_id, rec.pos, rec.pos + 1, index, insertion)
-
-
-
-                pileup_list = [base_list, indel_list, ad, dp, indel_deletion, indel_insertion]
+                pileup_list = [base_list, indel_list, ad, dp, re]
                 return pileup_list
 
             elif rec.pos == end - 1:
@@ -65,7 +63,6 @@ class BAM:
                 return None
 
     def fetch_row(self, bam_file, chr_id, start, end, index, indel_value):
-        # re = []
         i = 1
         for rec in bam_file.fetch(chr_id, start - 1 , end - 1):
             if i != index:
@@ -115,9 +112,14 @@ if __name__ == '__main__':
     f_d = '/home/cailei/bio_project/nbCNV/bam/SRR052047/SRR052047_rmduplicate.bam'
     b = BAM()
     bam_file = b.readfile(f_d)
-    re = b.fetch_row(bam_file, 'chr1', 31988447, 31988448, 2, -1)
+    # re = b.fetch_row(bam_file, 'chr1', 31988447, 31988448, 2, -1)
+    # print(re)
+    # 
+    # re = b.fetch_row(bam_file, 'chr1', 43785114, 43785115, 1, 1)
+    # print(re)
+
+    re = b.pileup_column(bam_file, 'chr1', 31988447, 31988448)
     print(re)
 
-    re = b.fetch_row(bam_file, 'chr1', 43785114, 43785115, 1, 1)
+    re = b.pileup_column(bam_file, 'chr1', 43785114, 43785115)
     print(re)
-
