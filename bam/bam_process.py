897ab88192bb4ca1976917eed72a25afc1835f75
CSuperlei <csuperlei@163.com>
2020-06-30 22:57:01 -0400

    2020-06-30

--- bam/bam_process.py
+++ bam/bam_process.py
@@ -42,12 +42,12 @@ class BAM:
                 if sum_indel_list == 0:  ## 如果是SNP
                     bl = rec.get_query_sequences()
                 elif sum_indel_list < 0:
-                    bl = son_pilup(bam_file, rec.pos + 1, rec.pos + 2)
+                    bl = son_pilup(bam_file, chr_id, rec.pos + 1, rec.pos + 2)
                     indel_index = np.argmin(indel_list)
                     indel_value = np.min(indel_list)
                     re = self.fetch_row(bam_file, chr_id, rec.pos, rec.pos + 1, indel_index, indel_value)
                 elif sum_indel_list > 0:
-                    bl = son_pilup(bam_file, rec.pos + 1, rec.pos + 2)
+                    bl = son_pilup(bam_file, chr_id, rec.pos + 1, rec.pos + 2)
                     indel_value = np.max(indel_list)
                     indel_index = np.argmax(indel_list)
                     re = self.fetch_row(bam_file, chr_id, rec.pos, rec.pos + 1, indel_index, indel_value)
