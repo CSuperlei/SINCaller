4cfb7200237002b1070de3641e050ee03f0e65dd
CSuperlei <csuperlei@163.com>
2020-07-01 18:27:35 -0400

    2020-07-01

--- data_process/data_preprocess.py
+++ data_process/data_preprocess.py
@@ -397,7 +397,7 @@ class DATAPROCESS:
             chr = rec[1]
             l_pos = int(rec[2]) - 1
             r_pos = int(rec[3]) - 1
-            for rec in bam_file.pileup(chr, l_pos, r_pos):
+            for rec in bam_file.pileup(chr, all):
                 try:
                     pos = rec.pos + 1  ## 参考基因位点
                     base_list = rec.get_query_sequences()
